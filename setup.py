9398a9a209b576883f468eb254ccb0be838d6909
Pengyuan Wei <53529144+pengyuanwei@users.noreply.github.com>
2025-02-01 11:18:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,30 +5,29 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-  name = 'space-time-astar',         # How you named your package folder (MyLib)
-  packages=setuptools.find_packages(),
-  version = '1.0',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A* search algorithm with an added time dimension to deal with dynamic obstacles.',   # Give a short description about your library
-  author = 'Pengyuan Wei',                   # Type in your name
-  author_email = 'ucabpw3@ucl.ac.uk',      # Type in your E-Mail
-  url = '',   # Provide either the link to your github or to your website
-  keywords = ['astar-algorithm', 'obstacle-avoidance', 'time-dimension'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'numpy',
-          'scipy',
-      ],
-  classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-  ],
-  python_requires='>=3.5',
-  long_description=long_description,
-  long_description_content_type='text/markdown'
-)
+    name = 'space-time-astar',         # How you named your package folder (MyLib)
+    version = '1.0',      # Start with a small number and increase it with every change you make
+    author = 'Haoran Peng',
+    author_email = 'gavinsweden@gmail.com',
+    maintainer = "Pengyuan Wei",
+    maintainer_email = "ucabpw3@ucl.ac.uk",
+    url = 'https://github.com/pengyuanwei/Space-Time-A-Star',   # Provide either the link to your github or to your website
+    description = 'A* search algorithm with an added time dimension to deal with dynamic obstacles.',   # Give a short description about your library
+    long_description = long_description,
+    long_description_content_type = 'text/markdown'    
 
-# python3 setup.py sdist bdist_wheel
-# twine upload dist/*
-# Tutorial at https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+    license = 'MIT',    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    keywords = ['multi-agent-path-finding', 'anonymous-agent', 'conflict-based-search', 'mapf', 'cbs', 'a-star'],   # Keywords that define your package best
+    packages = setuptools.find_packages(),
+    install_requires = [            # I get to this in a second
+        'numpy',
+        'scipy'
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+    ],
+    python_requires = '>=3.5',
+)
